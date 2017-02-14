ef5592c779ae24c83a2206006b3eae19552f0813
Eric Gonzalo <ersgonzalo@gmail.com>
2017-02-13 23:47:53 -0500

    Start work on Settings autoupdater and other minor changes

--- js/settings.js
+++ js/settings.js
@@ -5,18 +5,28 @@
 
 const settings = require('./app-settings.js');
 
-(function(){
-	//Populate settings from settings.js
-	populateValuesFromObject(settings.CONNECTION_SETTINGS);
-	populateValuesFromObject(settings.COPY_PATH);
+(function() {
+    //Populate settings from settings.js
+    populateValuesFromObject(settings.CONNECTION_SETTINGS);
+    populateValuesFromObject(settings.COPY_PATH);
 
-	function populateValuesFromObject(settingsObject){
-		console.log(settingsObject);
-		for(let idx in settingsObject){
-			var inputSetting = document.getElementById(idx);
-			if (inputSetting !== null){
-				inputSetting.value = settingsObject[idx];
-			}
-		}	
-	}
+    function populateValuesFromObject(settingsObject) {
+        console.log(settingsObject);
+        for (let idx in settingsObject) {
+            var inputSetting = document.getElementById(idx);
+            if (inputSetting !== null) {
+                inputSetting.value = settingsObject[idx];
+            }
+        }
+    }
+
+    watchInputsForUpdate();
+    function watchInputsForUpdate() {
+        let inputArray = document.getElementsByTagName('input');
+        for (let idx = 0; idx < inputArray.length - 1; idx++) {
+            inputArray[idx].addEventListener("keyup", () => {
+                console.log(inputArray[idx].value);
+            });
+        }
+    }
 })();
